bc780af656f87957615c3b0cd8619fc0a9753b60
ymjoz <rossiblack@duck.com>
2024-11-15 10:26:15 +0800

    chore: try promise

--- d2.js
+++ d2.js
@@ -1,12 +1,53 @@
 'use strict';
 
-console.log('同步before');
+console.log('test promise object');
 
-var obj = {
-  a: 1,
-  b: 2,
-};
-let arr_data = [2, 4, 6, 9, 10, 99, obj];
+/*
 
-const obj_keys = Object.keys(obj);
-console.log(arr_data.valueOf());
+const my_promise = new Promise((resolve, reject) => {
+  const runningResult = 1;
+  // throw new Error('promise 錯誤');
+  // 在這裡做一些 async 的工作
+
+  if (runningResult === 1) {
+    resolve('promise 回傳成功');
+  } else {
+    reject('promise 回傳失败');
+  }
+});
+
+my_promise
+  .then((result) => {
+    console.log('promise 結果:' + result);
+  })
+  .catch((err) => {
+    console.log('error:' + err);
+  })
+  .finally(() => {
+    console.log('finally');
+  });
+
+*/
+
+function timeOutFunc(ms) {
+  return new Promise((resolve, reject) => {
+    const runningResult = 9;
+    if (runningResult === 1) {
+      resolve('promise 回傳成功');
+    } else {
+      reject('promise 回傳失败');
+    }
+    setTimeout(resolve, ms, 'done!!');
+  });
+}
+
+timeOutFunc(3000)
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log('finally');
+  });
